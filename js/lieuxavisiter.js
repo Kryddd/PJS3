dfaa6095c023e5f4da659c5e09994b164e517106
yannchataignier <33687645+yannchataignier@users.noreply.github.com>
2017-11-29 12:30:04 +0100

    Update lieuxavisiter.js

--- js/lieuxavisiter.js
+++ js/lieuxavisiter.js
@@ -1,9 +1,14 @@
 var diapoIndex = 0;
 var diapos = document.getElementsByClassName("diapo");
 var points = document.getElementsByClassName("point");
-afficherDiapo(diapoIndex);
+afficherDiapo();
 
-function afficherDiapo(diapoIndex){
+function afficherDiapo(){
+	changerDiapo();
+	setTimeout(afficherDiapo, 3000,diapoIndex);
+}
+
+function changerDiapo(){
 	var i;
 	
 	for (i = 0; i < diapos.length; i++) {
@@ -13,24 +18,32 @@ function afficherDiapo(diapoIndex){
 	if (diapoIndex > diapos.length){
 		diapoIndex = 1;
 	}
+	
 	for (i = 0; i < points.length; i++) {
 		points[i].className = points[i].className.replace(" actif", "");
 	}
 	diapos[diapoIndex-1].style.display = "block";
 	points[diapoIndex-1].className += " actif";
-	setTimeout(afficherDiapo, 3000,diapoIndex);
 }
 
 function diapoSuivante(){
-	diapos[diapoIndex].style.display = "none";
-	points[diapoIndex].className = points[diapoIndex].className.replace(" actif", "");
-	diapos[diapoIndex+1].style.display = "block";
-	points[diapoIndex+1].className += " actif";
+	changerDiapo();
 }
 
 function diapoPrecedente(){
-	diapos[diapoIndex].style.display = "none";
-	points[diapoIndex].className = points[diapoIndex].className.replace(" actif", "");
+	var i;
+	
+	for (i = 0; i < diapos.length; i++) {
+		diapos[i].style.display = "none";
+	}
+	diapoIndex--;
+	if (diapoIndex < 1){
+		diapoIndex = 3;
+	}
+	
+	for (i = 0; i < points.length; i++) {
+		points[i].className = points[i].className.replace(" actif", "");
+	}
 	diapos[diapoIndex-1].style.display = "block";
 	points[diapoIndex-1].className += " actif";
 }
