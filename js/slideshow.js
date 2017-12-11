9aa90496286f436d1963a6191d34db1e4c879f32
PierreBaudoin <33687624+PierreBaudoin@users.noreply.github.com>
2017-12-11 15:16:20 +0100

    Add files via upload

--- js/slideshow.js
+++ js/slideshow.js
@@ -1,6 +1,7 @@
 var diapoIndex = 0;
 var diapos = document.getElementsByClassName("diapo");
 var points = document.getElementsByClassName("point");
+var nbBlocs = document.getElementsByClassName("blocDiapo");
 afficherDiapo();
 
 function afficherDiapo(){
@@ -15,15 +16,22 @@ function changerDiapo(){
 		diapos[i].style.display = "none";
 	}
 	diapoIndex++;
-	if (diapoIndex > diapos.length){
+	if (diapoIndex > 3){
 		diapoIndex = 1;
 	}
 	
 	for (i = 0; i < points.length; i++) {
 		points[i].className = points[i].className.replace(" actif", "");
 	}
+	
 	diapos[diapoIndex-1].style.display = "block";
 	points[diapoIndex-1].className += " actif";
+	
+	diapos[diapoIndex+2].style.display = "block";
+	points[diapoIndex+2].className += " actif";
+	
+	diapos[diapoIndex+5].style.display = "block";
+	points[diapoIndex+5].className += " actif";
 	
 	//afficherDiapo(5000);
 }
@@ -48,4 +56,10 @@ function diapoPrecedente(){
 	}
 	diapos[diapoIndex-1].style.display = "block";
 	points[diapoIndex-1].className += " actif";
+	
+	diapos[diapoIndex+2].style.display = "block";
+	points[diapoIndex+2].className += " actif";
+	
+	diapos[diapoIndex+5].style.display = "block";
+	points[diapoIndex+5].className += " actif";
 }
