fceea91e2981d6093dec6488e98e13949c1f1763
Kryddd <32571260+Kryddd@users.noreply.github.com>
2018-01-06 17:48:45 +0100

    modifs svg

--- js/slideshow.js
+++ js/slideshow.js
@@ -4,19 +4,19 @@ var points = document.getElementsByClassName("point");
 var nbBlocs = document.getElementsByClassName("blocDiapo");
 afficherDiapo();
 
-function afficherDiapo(){
+function afficherDiapo() {
 	changerDiapo();
 	setTimeout(afficherDiapo, 3000);
 }
 
-function changerDiapo(){
+function changerDiapo() {
 	var i;
 	
 	for (i = 0; i < diapos.length; i++) {
 		diapos[i].style.display = "none";
 	}
 	diapoIndex++;
-	if (diapoIndex > 3){
+	if (diapoIndex > 3) {
 		diapoIndex = 1;
 	}
 	
@@ -24,42 +24,42 @@ function changerDiapo(){
 		points[i].className = points[i].className.replace(" actif", "");
 	}
 	
-	diapos[diapoIndex-1].style.display = "block";
-	points[diapoIndex-1].className += " actif";
+	diapos[diapoIndex - 1].style.display = "block";
+	points[diapoIndex - 1].className += " actif";
 	
-	diapos[diapoIndex+2].style.display = "block";
-	points[diapoIndex+2].className += " actif";
+	diapos[diapoIndex + 2].style.display = "block";
+	points[diapoIndex + 2].className += " actif";
 	
-	diapos[diapoIndex+5].style.display = "block";
-	points[diapoIndex+5].className += " actif";
+	diapos[diapoIndex + 5].style.display = "block";
+	points[diapoIndex + 5].className += " actif";
 	
 	//afficherDiapo(5000);
 }
 
-function diapoSuivante(){
+function diapoSuivante() {
 	changerDiapo();
 }
 
-function diapoPrecedente(){
+function diapoPrecedente() {
 	var i;
 	
 	for (i = 0; i < diapos.length; i++) {
 		diapos[i].style.display = "none";
 	}
 	diapoIndex--;
-	if (diapoIndex < 1){
+	if (diapoIndex < 1) {
 		diapoIndex = 3;
 	}
 	
 	for (i = 0; i < points.length; i++) {
 		points[i].className = points[i].className.replace(" actif", "");
 	}
-	diapos[diapoIndex-1].style.display = "block";
-	points[diapoIndex-1].className += " actif";
+	diapos[diapoIndex - 1].style.display = "block";
+	points[diapoIndex - 1].className += " actif";
 	
-	diapos[diapoIndex+2].style.display = "block";
-	points[diapoIndex+2].className += " actif";
+	diapos[diapoIndex + 2].style.display = "block";
+	points[diapoIndex + 2].className += " actif";
 	
-	diapos[diapoIndex+5].style.display = "block";
-	points[diapoIndex+5].className += " actif";
+	diapos[diapoIndex + 5].style.display = "block";
+	points[diapoIndex + 5].className += " actif";
 }
